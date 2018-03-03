b1dd8a05171d70fdb91f4470e09768e7b737bb39
JIANGYIFENGPI <472231536@qq.com>
2018-03-03 07:30:23 +0200

    Add files via upload

--- python_django/tutorial/settings.py
+++ python_django/tutorial/settings.py
@@ -130,9 +130,16 @@ STATICFILES_DIRS = [
  'C:\Users\TLaw2\Downloads\PortableGit\python_django_rest\static',
 ]
 
+settings.py:
 REST_FRAMEWORK = {
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAdminUser',
-    ],
-    'PAGE_SIZE': 10
+ 'DEFAULT_RENDERER_CLASSES': [
+ 'rest_framework.renderers.JSONRenderer',
+ 'rest_framework.renderers.BrowsableAPIRenderer',
+ 'rest_framework_xml.renderers.XMLRenderer',
+ ],
+ 'DEFAULT_PERMISSION_CLASSES': [
+ 'rest_framework.permissions.IsAdminUser',
+ ],
+ 'PAGE_SIZE': 10
 }
+
--- python_django/tutorial/urls.py
+++ python_django/tutorial/urls.py
@@ -4,8 +4,13 @@ from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'groups', views.GroupViewSet)
-router.register(r'movies', views.MovieViewSet)
+router.register(r'courses', views.CoursesViewSet)
+router.register(r'curriculums', views.CurriculumsViewSet)
+router.register(r'groups', views.GroupsViewSet)
+router.register(r'implements', views.ImplementsViewSet)
+router.register(r'subgroups', views.subgroupsViewSet)
+router.register(r'rooms', views.RoomsViewSet)
+router.register(r'teachers', views.TeachersViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
